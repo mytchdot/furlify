d647608a9a6218cf3baf5375e15b1cb9c3875636
Mytch <mytchmason@gmail.com>
2023-08-30 18:35:03 -0600

    update deps, remove package from tests that no longer conforms to the same standards as the equally as popular packages

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,13 +1,5 @@
 import main from '..';
-const testLibs = [
-  '@babel/preset-env',
-  'axios',
-  'boxen',
-  'chalk',
-  'dexie',
-  'lodash',
-  'react',
-];
+const testLibs = ['axios', 'boxen', 'chalk', 'dexie', 'lodash', 'react'];
 describe('Main CLI Tool', () => {
   it('should test a list of modules from npm, running one package per call', async () => {
     for (let lib of testLibs) {
